0e01d9bc32664b02dcebb8e4d60d92b8247a4a5f
jsladovic <janko.sladovic@gmail.com>
2020-06-27 09:30:25 +0200

    Added camera extensions and vector extensions classes

---
+++ Assets/Scripts/CameraExtensions.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace FeelGoodOpgUtils
+{
+	public static class CameraExtensions
+	{
+		public static Vector3 MousePosition(this Camera camera)
+		{
+			Vector3 position = camera.ScreenToWorldPoint(Input.mousePosition);
+			position.z = 0.0f;
+			return position;
+		}
+
+		public static bool IsMouseOverCamera(this Camera camera)
+		{
+			return camera.pixelRect.Contains(Input.mousePosition) == true;
+		}
+	}
+}
