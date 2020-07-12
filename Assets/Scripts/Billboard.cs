afeca4b8ac1576d484d4cafc1ff91f71938d5b34
jsladovic <janko.sladovic@gmail.com>
2020-07-12 12:58:51 +0200

    Moved billboard to feel good opg utils

---
+++ Assets/Scripts/Billboard.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace FeelGoodOpgUtils
+{
+	public class Billboard : MonoBehaviour
+	{
+		public Transform CameraTransform;
+		private Transform CanvasTransform;
+		private Quaternion OriginalRotation;
+
+		private void Start()
+		{
+			if (CameraTransform == null)
+				CameraTransform = Camera.main.transform;
+
+			CanvasTransform = GetComponent<Canvas>()?.transform;
+			if (CanvasTransform == null)
+				throw new UnityException("Can't find canvas on object");
+			OriginalRotation = CanvasTransform.rotation;
+		}
+
+		private void Update()
+		{
+			CanvasTransform.rotation = CameraTransform.rotation * OriginalRotation;
+		}
+	}
+}
