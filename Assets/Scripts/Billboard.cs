676214a681b732f9990ec60863acd91a7e1666db
jsladovic <janko.sladovic@gmail.com>
2020-09-29 23:04:04 +0200

    Billboard changes

--- Assets/Scripts/Billboard.cs
+++ Assets/Scripts/Billboard.cs
@@ -5,23 +5,19 @@ namespace FeelGoodOpgUtils
 	public class Billboard : MonoBehaviour
 	{
 		public Transform CameraTransform;
-		private Transform CanvasTransform;
 		private Quaternion OriginalRotation;
 
 		private void Start()
 		{
 			if (CameraTransform == null)
 				CameraTransform = Camera.main.transform;
-
-			CanvasTransform = GetComponent<Canvas>()?.transform;
-			if (CanvasTransform == null)
-				throw new UnityException("Can't find canvas on object");
-			OriginalRotation = CanvasTransform.rotation;
+			
+			OriginalRotation = transform.rotation;
 		}
 
 		private void Update()
 		{
-			CanvasTransform.rotation = CameraTransform.rotation * OriginalRotation;
+			transform.rotation = CameraTransform.rotation * OriginalRotation;
 		}
 	}
 }
