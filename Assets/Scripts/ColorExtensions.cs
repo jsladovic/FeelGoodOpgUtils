a1c5c6caea9bda558c0d21e7d9c8502dae3f1749
sparrowich <mihaela4295@gmail.com>
2020-10-11 16:53:35 +0200

    Added color extensions

---
+++ Assets/Scripts/ColorExtensions.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace FeelGoodOpgUtils
+{
+	public static class ColorExtensions
+	{
+		public static Color With(this Color color, float? r = null, float? g = null, float? b = null, float? a = null)
+		{
+			return new Color(r ?? color.r, g ?? color.g, b ?? color.b, a ?? color.a);
+		}
+	}
+}
